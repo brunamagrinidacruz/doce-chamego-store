d2b876017ec3d6727a70101f0fe14baeee1d6ecb
mjmartins11 <realmartins11@gmail.com>
2021-06-27 19:13:15 -0300

    Atualizaçao dos arquivos administraçao para admin

---
+++ vue/admin.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+    el: "#container",
+    
+    data: {
+        produto: [ 
+            {
+                nome: "Cone trufado",
+                descricao: "Uma deliciosa sobremesa",
+                preco: "20,00",
+                quantidade: 5,
+                prazoMin: 15,
+                prazoMax: 20
+            },
+            {
+                nome: "Cesta de guloseimas",
+                descricao: "Uma deliciosa sobremesa",
+                preco: "75,00",
+                quantidade: 5,
+                prazoMin: 15,
+                prazoMax: 20
+            }
+        ]
+    }
+    
+    methods: {
+        removerProduto() {
+            
+        }
+    }
+
+
+})
