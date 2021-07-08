83c0ffeb092d411fd97238e35b77bda18a03c7ab
mjmartins11 <realmartins11@gmail.com>
2021-07-07 21:24:11 -0300

    verificaçao de inserçao de letra na quantidade do carrinho

--- vue/carrinho.js
+++ vue/carrinho.js
@@ -50,10 +50,10 @@ var app = new Vue({
     },
 
     watch: {
-        'quantidadeDosProdutos': function (novoValor, valorAntigo) {
+        'quantidadeDosProdutos': function (novoValor) {
             let erro = false;
             for(let i = 0; i < novoValor.length; i++) {
-                if(novoValor[i] < 0 || novoValor[i] > 30) {
+                if(novoValor[i] < 0 || novoValor[i] > 30 || novoValor[i] == "") {
                     erro = true;
                     this.quantidadeDosProdutos[i] = 1;
                 }
@@ -109,15 +109,3 @@ var app = new Vue({
         }
     }
 })
-
-function isNumber(numero) {
-    let numerotxt = String(numero)
-    for (let i = 0; i < numerotxt.length; i++) {
-        let code = numerotxt.charCodeAt(i);
-        if (code <= 48 || code >= 57) {          
-            numerotxt.value=""; 
-            return false;
-        }
-    }
-    return true;
-}
