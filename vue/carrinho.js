83f7312ec915518d13300501cf51ee2106822059
mjmartins11 <realmartins11@gmail.com>
2021-07-07 21:59:11 -0300

    Validaçao de vazio no numero de quantidade do carrinho

--- vue/carrinho.js
+++ vue/carrinho.js
@@ -35,6 +35,7 @@ var app = new Vue({
             },
         erros: []
     },
+
     beforeCreate() {
         valorTotal = 0,
         qtdDeProdutos = 0,
@@ -53,7 +54,7 @@ var app = new Vue({
         'quantidadeDosProdutos': function (novoValor) {
             let erro = false;
             for(let i = 0; i < novoValor.length; i++) {
-                if(novoValor[i] < 0 || novoValor[i] > 30 || novoValor[i] == "") {
+                if(novoValor[i] < 0 || novoValor[i] > 30 || !isNumber(novoValor[i])) {
                     erro = true;
                     this.quantidadeDosProdutos[i] = 1;
                 }
@@ -71,15 +72,16 @@ var app = new Vue({
             _vm.qtdDeProdutos -= this.quantidadeDosProdutos[indice];
             _vm.produto.splice(indice, 1);
         },
+
         entrar() {
             this.erros = [];
 
             if(!this.formaDePagamento.nomeDoTitular) {
-                  this.erros.push("Digite o nome do titular.");
+                this.erros.push("Digite o nome do titular.");
             }
 
             if(!this.formaDePagamento.numeroDoCartao || this.formaDePagamento.numeroDoCartao.length !== 16) {
-                  this.erros.push("Digite o numero do cartao.");
+                this.erros.push("Digite o numero do cartao.");
             }
             if(!this.formaDePagamento.cvv || this.formaDePagamento.cvv.length !== 3) {
                 this.erros.push("Digite o cvv.");
@@ -99,13 +101,39 @@ var app = new Vue({
                 }
             }
         },
+
         precoTotal(){
+            let valorTotalAntigo = this.valorTotal;
+            let qtdDeProdutosAntigo = this.qtdDeProdutos;
+
             this.valorTotal = 0;
             this.qtdDeProdutos = 0;
-            for (let indice = 0; indice < this.produto.length; indice++) {
+
+            let indice;
+            for (indice = 0; indice < this.produto.length && this.quantidadeDosProdutos[indice] != ""; indice++) {
                 this.valorTotal += (this.produto[indice].preco * this.quantidadeDosProdutos[indice]);
                 this.qtdDeProdutos += parseInt(this.quantidadeDosProdutos[indice], 10);
+            }
+
+            //console.log(indice, this.produto.length)
+            if (indice < this.produto.length) { // existe produto com o número de quantidade vazio
+                //console.log("erro")
+                this.valorTotal = valorTotalAntigo;
+                this.qtdDeProdutos = qtdDeProdutosAntigo;
+                alert("Insira uma quantidade valida para o(s) produto(s)!");
             }
         }
     }
 })
+
+function isNumber(numero) {
+    let numerotxt = String(numero)
+    for (let i = 0; i < numerotxt.length; i++) {
+        let code = numerotxt.charCodeAt(i);
+        if (code <= 48 || code >= 57) {          
+            numerotxt.value=""; 
+            return false;
+        }
+    }
+    return true;
+}
