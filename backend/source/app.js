db132c37d5e76b5a862256d625aa36616c9498bb
Wellington <amaral_wellington@hotmail.com>
2021-07-18 19:04:22 -0300

    primeiros metodos do protudo-controller

--- backend/source/app.js
+++ backend/source/app.js
@@ -5,10 +5,12 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const usuarioRoute = require('./routes/usuario-route.js');
+const produtoRoute = require('./routes/produto-route.js');
 
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(bodyParser.json({extended: false}));
 
 app.use('/usuario', usuarioRoute);
+app.use('/produto', produtoRoute);
 
 module.exports = app;
--- backend/source/controllers/produto-controller.js
+++ backend/source/controllers/produto-controller.js
@@ -12,7 +12,7 @@ exports.get = async(req, res, next) => {
         res.status(200).send(data);
     } catch (e) {
         res.status(500).send({
-            message: 'Falha ao processar sua requisição'
+            mensagem: 'Falha ao processar sua requisição'
         });
     }
 }
@@ -23,7 +23,33 @@ exports.getById = async(req, res, next) => {
         res.status(200).send(data);
     } catch (e) {
         res.status(500).send({
+            mensagem: 'Falha ao processar sua requisição'
+        });
+    }
+}
+
+exports.put = async(req, res, next) => {
+    try {
+        await repository.update(req.params.id, req.body);
+        res.status(200).send({
+            mensagem: 'Produto atualizado com sucesso!'
+        });
+    } catch (e) {
+        res.status(500).send({
             message: 'Falha ao processar sua requisição'
+        });
+    }
+}
+
+exports.delete = async() => {
+    try {
+        await repository.delete(req.params.id);
+        res.status(200).send({
+            mensagem: 'Produto excluido com sucesso!'
+        });
+    } catch (e) {
+        res.status(500).send({
+            mensagem: 'Falha ao processar sua requisicao'
         });
     }
 }
