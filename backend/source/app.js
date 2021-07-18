ee826a33047a88a3563f1e6202dddadc37cc3001
Bruna Magrini <magrinibruna@usp.br>
2021-07-18 18:56:53 -0300

    repository

--- backend/source/app.js
+++ backend/source/app.js
@@ -10,7 +10,17 @@ const app = express();
 // Conexão com o MongoDB
 mongoose.connect(config.connectionString);
 
-const Produto = require('./models/produto')
+const Produto = require('./models/produto');
+
+const repository = require('./repositories/produto-repository');
+// repository.put({
+//       nome: "Cone Trufado (Maracuja)",
+//       foto: "http://",
+//       descricao: "Esse é o mais forte produto do nosso ramo, está conosco desde o primórdio. Descubra este novo sabor!",
+//       preco: 7.90,
+//       quantidadeEstoque: 10,
+//       quantidadeVendida: 5
+// })
 
 const usuarioRoute = require('./routes/usuario-route.js');
 
--- backend/source/config.js
+++ backend/source/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-      connectionString: 'mongodb+srv://doce-chamego:doce-chamego@cluster-loja-doce-chame.0cgox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+      connectionString: 'mongodb+srv://doce-chamego:doce-chamego@cluster-loja-doce-chame.0cgox.mongodb.net/loja-doce-chamego?retryWrites=true&w=majority'
 }
