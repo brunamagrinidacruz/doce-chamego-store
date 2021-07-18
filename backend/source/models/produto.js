048fc5d8d074fe5d94fc11e65ef0cf0df2431bcb
Bruna Magrini <magrinibruna@usp.br>
2021-07-18 18:35:34 -0300

    iniciada conexao com mongodb

---
+++ backend/source/models/produto.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+      nome: {
+            type: String,
+            required: true,
+            index: true
+      },
+      foto: {
+            type: String,
+            required: true
+      },
+      descricao: {
+            type: String,
+            required: true
+      },
+      preco: {
+            type: Number,
+            required: true,
+      },
+      quantidadeEstoque: {
+            type: Number,
+            required: true
+      },
+      quantidadeVendida: {
+            type: Number,
+            required: true
+      }
+})
+
+module.exports = mongoose.model('Produto', schema);
