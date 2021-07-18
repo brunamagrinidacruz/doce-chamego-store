12f52c204fc35d989abc2d463edfa2ac626e72fd
Bruna Magrini <magrinibruna@usp.br>
2021-07-18 19:48:14 -0300

    repositorios

--- backend/source/repositories/produto-repository.js
+++ backend/source/repositories/produto-repository.js
@@ -4,24 +4,39 @@ const mongoose = require('mongoose');
 const Produto = mongoose.model('Produto');
 
 exports.get = async() => {
-      console.log("get produto");
-
       const res = await Produto.find();
       return res;
 }
 
 exports.post = async(data) => {
-      console.log("post produto");
-
       let produto = new Produto(data);
       await produto.save();
 }
   
-exports.put = async(data) => {
-      console.log("put produto");
+exports.put = async(id, data) => {
+      await Produto
+            .findByIdAndUpdate(id, {
+                  $set: {
+                        nome: data.nome,
+                        foto: data.foto,
+                        descricao: data.descricao,
+                        preco: data.preco,
+                        quantidadeEstoque: data.quantidadeEstoque,
+                        quantidadeVendida: data.quantidadeVendida,
+                  }
+            });
 }
 
-exports.delete = async(data) => {
-      console.log("delete produto");
+exports.delete = async(id) => {
+      await Produto.findOneAndRemove(id);
 }
-  
+
+exports.sell = async(id, data) => {
+      await Produto
+            .findByIdAndUpdate(id, {
+                  $set: {
+                        quantidadeEstoque: data.quantidadeEstoque,
+                        quantidadeVendida: data.quantidadeVendida,
+                  }
+            });
+}
--- backend/source/repositories/usuario-repository.js
+++ backend/source/repositories/usuario-repository.js
@@ -4,23 +4,48 @@ const mongoose = require('mongoose');
 const Usuario = mongoose.model('Usuario');
 
 exports.get = async() => {
-      console.log("get usuario");
-      
       const res = await Usuario.find();
       return res;
 }
 
 exports.post = async(data) => {
-      console.log("post usuario");
-
       let usuario = new Usuario(data);
       await usuario.save();
 }
   
-exports.put = async(data) => {
-      console.log("put usuario");
+exports.put = async(id) => {
+      await Usuario
+            .findByIdAndUpdate(id, {
+                  $set: {
+                        cpf: data.cpf,
+                        nome: data.nome,
+                        endereco: data.endereco,
+                        telefone: data.telefone,
+                        email: data.email,
+                        senha: data.senha,
+                        ehAdministrador: data.ehAdministrador
+                  }
+            });
 }
 
-exports.delete = async(data) => {
-      console.log("delete usuario");
+exports.delete = async(id) => {
+      await Usuario.findOneAndRemove(id);
+}
+  
+exports.authenticate = async(data) => {
+      const res = await Usuario.findOne({
+          email: data.email,
+          password: data.password
+      });
+      return res;
+}
+
+exports.ehAdministrador = async(data) => {
+      const res = await Usuario.findOne({
+            email: data.email
+      });
+
+      console.log(res);
+
+      return res;
 }
