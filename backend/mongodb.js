ea03bdff0f767edc07c40612ade1a56714faeb6c
Bruna Magrini <magrinibruna@usp.br>
2021-07-18 18:37:40 -0300

    conexao com mongodb

--- backend/mongodb.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://doce-chamego:doce-chamego@cluster-loja-doce-chame.0cgox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
