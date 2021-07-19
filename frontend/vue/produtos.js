39e2e458a906db1f0339eb9595aabe22d03178c9
Bruna Magrini <magrinibruna@usp.br>
2021-07-18 21:15:50 -0300

    conexao backend e frontend: tela de listagem da administracao de produtos e usuarios

--- frontend/vue/produtos.js
+++ frontend/vue/produtos.js
@@ -2,31 +2,38 @@ var app = new Vue({
     el: "#app",
     
     data: {
-        produtos: [ 
-            {
-                nome: "Cone trufado",
-                descricao: "Uma deliciosa sobremesa",
-                preco: "20,00",
-                quantidade: 5,
-                prazoMin: 15,
-                prazoMax: 20
-            },
-            {
-                nome: "Cesta de guloseimas",
-                descricao: "Hmmmm, mas que gostoso!",
-                preco: "75,00",
-                quantidade: 17,
-                prazoMin: 15,
-                prazoMax: 20
-            }
-        ]
+        produtos: []
     },
     
     methods: {
         excluir: function(indice) {
             this.produtos.splice(indice, 1);
         }
-    }
+    },
+
+    mounted() {
+        this.$nextTick(function () {
+              fetch('http://localhost:3000/produto')
+              .then(response => {
+                          // valida se a requisição falhou
+                          if (!response.ok) {
+                                return new Error('falhou a requisição') // cairá no catch da promise
+                          }
+
+                          // verificando pelo status
+                          if (response.status === 404) {
+                                return new Error('não encontrou qualquer resultado')
+                          }
+
+                          // retorna uma promise com os dados em JSON
+                          return response.json()
+                    })
+              .then(data => {
+                    this.produtos = data;
+              })
+              .catch(err => console.log(err.message))
+        })
+  }
 
 
 })
